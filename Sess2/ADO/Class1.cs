a2cae68e820442a87e1e621829e3fb18fafeefbb
Quiet-52 <81682632+Quiet-52@users.noreply.github.com>
2023-05-27 11:32:50 +0300

    Add files via upload

---
+++ Sess2/ADO/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sess2.ADO
+{
+    public static class AppData
+    {
+        public static Session1Entities BD = new Session1Entities();
+    }
+}
